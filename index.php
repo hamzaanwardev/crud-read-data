cd11b3477a1cd8ba6d29aa57650a77e2ba5347ef
Hamza Anwar <88555920+hamzaanwardev@users.noreply.github.com>
2021-11-18 01:45:04 +0500

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,17 @@
 include 'header.php';
 ?>
 <div id="main-content">
+<h2>All Records</h2>
     <?php
-        $conn = mysqli_connect("localhost","root","","crud") or die("Connection Failed");
+        $conn = mysqli_connect("localhost","root","","my_first_db") or die("Connection Failed");
+
+        $sql = "SELECT * FROM student JOIN studentsclass WHERE students.s_class = studentsclass.cid";
+
+        $result = mysqli_query($conn, $sql) or die("Query Unsuccessful.");
+
+        if(mysqli_num_rows($result) > 0 ) {
     ?>
-    <h2>All Records</h2>
+    
     <table cellpadding="7px">
         <thead>
         <th>Id</th>
@@ -73,6 +80,7 @@ include 'header.php';
             </tr>
         </tbody>
     </table>
+    <?php } ?>
 </div>
 </div>
 </body>
