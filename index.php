a78c160dc852a095c6cc3a5698146ae94b15a968
Hamza Anwar <88555920+hamzaanwardev@users.noreply.github.com>
2021-11-19 01:30:25 +0500

    Update index.php

--- index.php
+++ index.php
@@ -29,11 +29,11 @@ include 'header.php';
 
             <tr>
 
-                <td>1</td>
-                <td>Ramesh</td>
-                <td>Delhi</td>
-                <td>BCA</td>
-                <td>9876543210</td>
+                <td><?php echo $row['sid']; ?></td>
+                <td><?php ?></td>
+                <td><?php ?></td>
+                <td><?php ?></td>
+                <td><?php ?></td>
                 <td>
                     <a href='edit.php'>Edit</a>
                     <a href='delete-inline.php'>Delete</a>
