c4e0ed9467a5c386ed8d59736702ac7c236e2f9c
Hamza Anwar <88555920+hamzaanwardev@users.noreply.github.com>
2021-11-21 00:50:01 +0500

    saving data to savedata.php and redirecting page to index.php after saving data.

--- savedata.php
+++ savedata.php
@@ -1,9 +1,15 @@
 <?php
 
-$stu_name = $_POST['sname'];
-$stu_address = $_POST['saddress'];
-$stu_class = $_POST['class'];
-$stu_phone = $_POST['sphone'];
+echo $stu_name = $_POST['sname'];
+echo $stu_address = $_POST['saddress'];
+echo $stu_class = $_POST['class'];
+echo $stu_phone = $_POST['sphone'];
 
+$conn = mysqli_connect("localhost","root","","my_first_db") or die("Connection Failed");
 
+$sql = "INSERT INTO students(s_name,s_address,s_class,s_phone) VALUES ('{$stu_name}', '{$stu_address}', '{$stu_class}','{$stu_phone}')";
+
+$result = mysqli_query($conn, $sql) or die("Query Unsuccessful.");
+
+header("Location: http://localhost/Git/crud-read-data/index.php");
 ?>
