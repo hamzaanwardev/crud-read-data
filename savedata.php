91402aa21aeae8cf9077ea1502ce217dbb3ef9ff
Hamza Anwar <88555920+hamzaanwardev@users.noreply.github.com>
2021-11-21 00:36:47 +0500

    Update savedata.php

--- savedata.php
+++ savedata.php
@@ -1,9 +1,9 @@
 <?php
 
-$stu_name = $_POST['s_name'];
-$stu_address = $_POST['s_address'];
-$stu_class = $_POST['s_class'];
-$stu_phone = $_POST['s_phone'];
+$stu_name = $_POST['sname'];
+$stu_address = $_POST['saddress'];
+$stu_class = $_POST['class'];
+$stu_phone = $_POST['sphone'];
 
 
 ?>
