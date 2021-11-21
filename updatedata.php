fc7cc361d4151b706fda3cce6c1e8db806091e0f
Hamza Anwar <88555920+hamzaanwardev@users.noreply.github.com>
2021-11-21 11:12:03 +0500

    Another Method of updating data with update button

--- updatedata.php
+++ updatedata.php
@@ -13,7 +13,7 @@ $stu_phone = $_POST['sphone'];
 // making connection
 $conn = mysqli_connect("localhost","root","","my_first_db") or die("Connection Failed");
 
-$sql = "UPDATE students SET s_name = '{$stu_name}', s_address = '{$stu_address}', s_class = '{$stu_class}', s_phone = '{$stu_phone}' WHERE sid = {$stu_id}";
+$sql = "UPDATE students SET s_name = '{$stu_name}', s_address = '{$stu_address}', s_class = '{$stu_class}', s_phone = '{$stu_phone}' WHERE sid = '{$stu_id}'";
 
 $result = mysqli_query($conn, $sql) or die("Query Unsuccessful.");
 
